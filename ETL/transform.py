092f2a611753e570e44f97e24d00ef4a74c513e3
WhiteW00lf <mocha_cookie_crumble@proton.me>
2025-07-08 04:44:24 +0000

    Updated transform.py

--- ETL/transform.py
+++ ETL/transform.py
@@ -7,31 +7,23 @@ def read_csv():
     Reads the weather data from a CSV file and returns a DataFrame.
     """
     try:
-        df = pd.read_csv(
-            f"../data/{datetime.today().date()}_weather_data.csv",
-            header=None,
-            names=[
-                "created_at",
-                "temp",
-                "humidity",
-                "description",
-                "clouds",
-                "wind_speed",
-                "pressure",
-            ],
-        )
+        df = pd.read_csv("airquality.csv", parse_dates=["created_at"])
         print("Data read successfully from weather_data.csv")
         df.dropna(inplace=True)
 
         # Transforming dataframe to a meaningful format
         df["created_at"] = pd.to_datetime(df["created_at"])
-        df["temp"] = df["temp"].astype(int)
-        df["humidity"] = df["humidity"].astype(float)
-        df["clouds"] = df["clouds"].astype(float)
-        df["wind_speed"] = df["wind_speed"].astype(float)
-        df["pressure"] = df["pressure"].astype(float)
+        df["co"] = df["co"].astype(float)
+        df["no"] = df["no"].astype(float)
+        df["no2"] = df["no2"].astype(float)
+        df["o3"] = df["o3"].astype(float)
+        df["so2"] = df["so2"].astype(float)
+        df["pm2_5"] = df["pm2_5"].astype(float)
+        df["pm10"] = df["pm10"].astype(float)
+        df["nh3"] = df["nh3"].astype(float)
+        df["aqi"] = df["aqi"].astype(int)
         return df
-        print(df.dtypes)
+        
     except FileNotFoundError:
         print("File not found. Please ensure the file exists.")
         return pd.DataFrame()  # Return an empty DataFrame if the file does not exist
