ae8910a483fc3389bfa7071750873220e20634da
WhiteW00lf <mocha_cookie_crumble@proton.me>
2025-07-09 04:33:21 +0000

    fixed key error in load.py

--- dags/ETL/load.py
+++ dags/ETL/load.py
@@ -1,5 +1,6 @@
 import boto3
 import os
+from botocore.exceptions import ClientError
 
 s3 = boto3.client("s3")
 
@@ -19,14 +20,17 @@ def upload_to_s3():
     
     bucket_name = "aqmumbaipipeline"  # Replace with your S3 bucket name
     file_name = "/home/ubuntu/DE_projects/Air-Quality-Pipeline/data/airquality.csv"
+    s3_key = 'airquality.csv'
     try:
-        s3.head_object(Bucket=bucket_name, Key=file_name)
-        print(f"File {file_name} already exists in bucket {bucket_name}.")
-        s3.delete_object(Bucket=bucket_name, Key=file_name)
-        print(f"Deleted old file - {file_name} from bucket {bucket_name}.")
-        print(f"Uploading new file - {file_name} to bucket {bucket_name}.")
-        s3.upload_file(file_name, bucket_name, file_name)
-        print(f"File {file_name} uploaded to S3 bucket {bucket_name} successfully.")
+        s3.head_object(Bucket=bucket_name, Key=s3_key)
+        print(f"File {s3_key} already exists in bucket {bucket_name}.")
+        s3.delete_object(Bucket=bucket_name, Key=s3_key)
+    except s3.exceptions.ClientError:
+        print(f"File {s3_key} does not exist in bucket {bucket_name}. Proceeding to upload.")
+        print(f"Deleted old file - {s3_key} from bucket {bucket_name}.")
+        print(f"Uploading new file - {s3_key} to bucket {bucket_name}.")
+        s3.upload_file(file_name, bucket_name, s3_key)
+        print(f"File {s3_key} uploaded to S3 bucket {bucket_name} successfully.")
     except Exception as e:
         print(f"Failed to upload file to S3: {e}")
 
